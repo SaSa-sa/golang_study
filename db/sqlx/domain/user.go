bc744c5197987cc4c54acf58dfb0575b7a389b5f
SaSa-sa <57384326+SaSa-sa@users.noreply.github.comh>
2020-04-02 18:10:16 +0900

    sqlx使用ver

--- db/sqlx/domain/user.go
+++ db/sqlx/domain/user.go
@@ -1,9 +1,9 @@
 package domain
 
 // User is a struct for user table.
-type User struct {
+type User struct { //※ユーザーテーブルのどのカラムが、structのどのカラムに値するか
 	// sqlxのdbタグを使ってカラムとフィールドをマッピングする
-	ID        int    `db:"id"`
+	ID        int    `db:"id" json:"id"` //...みたいにjsonの値も定義できる？(jbuilder的な) addほにゃららってコマンド叩くらしい
 	FirstName string `db:"first_name"`
 	LastName  string `db:"last_name"`
 }
